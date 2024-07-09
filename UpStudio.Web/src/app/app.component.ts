1267edbaa567c383c757d2e3d1f9475b18af326d
chayush <362chaya@gmail.com>
2024-07-09 22:48:05 +0300

    hopen the component system classes

--- UpStudio.Web/src/app/app.component.ts
+++ UpStudio.Web/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SystemClassesComponent } from './components/system-classes/system-classes.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SystemClassesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
