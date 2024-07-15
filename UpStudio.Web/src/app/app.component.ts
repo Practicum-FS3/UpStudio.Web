36d49f537b8eba7281c1113195ad1413834be377
unknown <chz0556754500@gmail.com>
2024-07-15 12:35:46 +0300

    create component health-funds html

--- UpStudio.Web/src/app/app.component.ts
+++ UpStudio.Web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HealthFundsComponent } from './components/health-funds/health-funds.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HealthFundsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
