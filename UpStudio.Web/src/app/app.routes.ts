4685ac0b788f7209d077f18c0cbdaf45e657fb81
AvigailValnin <avigailvkn@gmail.com>
2024-07-15 12:37:23 +0300

    create component PaymentsOption html

--- UpStudio.Web/src/app/app.routes.ts
+++ UpStudio.Web/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaymentsOptionsComponent } from './payments-options/payments-options.component';
+const routes: Routes = [
+  { path: '', component:  PaymentsOptionsComponent}, // ברירת מחדל לקומפוננטת הבית
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
