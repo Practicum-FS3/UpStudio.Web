81abf53a7d201ca3275ef8a1bb7231edd5e62f8f
BatyaMamman <batyam5014@gmail.com>
2024-07-17 23:26:04 +0300

    HTML-Paying-Options-Component

---
+++ UpStudio.Web/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HealthFundsComponent } from './components/health-funds/health-funds.component';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { SubscriptionBenefitsComponent } from '../subscription-benefits/subscription-benefits.component';
+import { PaymentsOptionsComponent } from './components/payments-options/payments-options.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet 
+            ,RouterModule
+           ,HealthFundsComponent
+            , SubscriptionBenefitsComponent,
+          PaymentsOptionsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'UpStudio.Web';
+}
---
+++ UpStudio.Web/src/app/app.routes.ts
@@ -0,0 +1,32 @@
+// import { Routes } from '@angular/router';
+
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PaymentsOptionsComponent } from './payments-options/payments-options.component';
+const routes: Routes = [
+  { path: '', component:  PaymentsOptionsComponent}, // ברירת מחדל לקומפוננטת הבית
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routes: Routes = [
+    // { path: "", redirectTo:"home" ,pathMatch:"full"},
+    // { path: "home" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "?????" ,component:?????},
+    // { path: "**" ,component:?????},
+    { path: 'price list', component:  PaymentsOptionsComponent}, // ברירת מחדל לקומפוננטת הבית
+
+];
+
