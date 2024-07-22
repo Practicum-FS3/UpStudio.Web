ee3bace1d45c989dce862be24f442421ba2a263d
Michal Chaimson <michalch7676@gmail.com>
2024-07-22 09:07:55 +0300

    Add models of content

--- UpStudio.Web/src/app/app.routes.ts
+++ UpStudio.Web/src/app/app.routes.ts
@@ -4,19 +4,16 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaymentsOptionsComponent } from './payments-options/payments-options.component';
-const routes: Routes = [
-  { path: '', component:  PaymentsOptionsComponent}, // ברירת מחדל לקומפוננטת הבית
-];
+import { PaymentsOptionsComponent } from '../app/components/payments-options/payments-options.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //imports: [RouterModule.forRoot(Routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
 
 export const routes: Routes = [
-    // { path: "", redirectTo:"home" ,pathMatch:"full"},
+   { path: "", redirectTo:"home" ,pathMatch:"full"},
     // { path: "home" ,component:?????},
     // { path: "?????" ,component:?????},
     // { path: "?????" ,component:?????},
